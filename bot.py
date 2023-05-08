9703e8ce9bfb07c8666b8adcb77bb05ca582c6c5
RachhaAshwin <99497220+RachhaAshwin@users.noreply.github.com>
2023-05-08 10:43:37 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,58 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from transformers import pipeline
+from gradio_client import Client
+import requests
+
+#client = Client("https://mosaicml-mpt-7b-chat--thv27.hf.space/")
+
+os.environ["SLACK_BOT_TOKEN"] = "xoxb-5248486729728-5237633601905-tuQbUMOAc8pvgyWT4pnRQJrw"
+os.environ["SLACK_APP_TOKEN"] = "xapp-1-A056EBBQC4W-5210458109047-9dced026191e007d716e7cc1aed24bea729bfab6c21a6636a8eec0d713227963"
+
+def generate_text(text):
+    ans = "Server Error"
+
+    response = requests.post("https://tloen-alpaca-lora.hf.space/run/predict", json={
+        "data": [
+            text,
+            "Explain this code to me : ",
+            0,
+            0.75,
+            40,
+            4,
+            300,
+        ]
+    }).json()
+    if "data" not in response:
+        return ans
+    data = response['data'][0]
+    print(data)
+    ans = f"Output : {data}"
+    return ans
+
+
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+
+@app.command("/explain_code")
+def handle_explain_code(ack, body, logger):
+    ack()
+    text = " ".join(body["text"].split()[1:])
+    if not text.strip():
+        return "Please provide some text to explain."
+    generated_text = generate_text(text)
+    response = f"Explanation for '{text}':\n{generated_text}"
+    return response
+
+@app.event("message")
+def handle_message(event, say):
+    text = event["text"]
+    if not text.strip():
+        say("Please provide some text to explain.")
+        return
+    generated_text = generate_text(text)
+    say(generated_text)
+
+
+handler = SocketModeHandler(app_token=os.environ["SLACK_APP_TOKEN"], app=app)
+handler.start()
